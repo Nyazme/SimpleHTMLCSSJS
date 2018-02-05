96a521641c4c14d3dd61a07ef0b3843cfa18738e
Mark Feinman <feinmanmarke@gmail.com>
2018-02-05 16:06:49 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,36 @@
+console.log("Meow");
+
+var Chewy = 5;
+var solo = 'Han';
+var falcon = true;
+
+if(falcon == true) {
+    console.log("Totally true");
+} else {
+    console.log ("Totes not true");
+}for(var i=0; i<10; i ++){
+  console.log("Here: " , i);
+}
+var scott = {};
+scott.age = 18;
+scott.name = "Scott Bromander";
+scott.hair = "Brown";
+
+function something () {
+    console.log("Yao");
+    return "Yao!";
+}
+var doesThisWork = something();
+console.log(doesThisWork);
+doesThisWork = 10;
+doesThisWork = "Wha TTHE HELL!?"
+console.log (doesThisWork);
+
+doesThisWork = function(){
+    doesThisWork = 42;
+    };
+doesThisWork();
+console.log(doesThisWork);
+
+var ann = document.getElementById("cathryn").innerText="Does this work";
+
